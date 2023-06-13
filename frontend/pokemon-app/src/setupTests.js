5ea1767b982cac078a3b7d9c809b9c713ff689cd
minatogawa <minatogawavlf@gmail.com>
2023-06-13 19:20:56 -0400

    updated setupTests.js

--- frontend/pokemon-app/src/setupTests.js
+++ frontend/pokemon-app/src/setupTests.js
@@ -1,5 +1,15 @@
+// setupTests.js
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+const crypto = require('crypto');
+
+window.crypto = {
+  subtle: crypto.webcrypto.subtle,
+  getRandomValues: function(array) {
+    return crypto.randomFillSync(array);
+  },
+};
