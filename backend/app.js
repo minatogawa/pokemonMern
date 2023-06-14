330d64d9deb4892b794a89d4a63023f727b9b6d7
minatogawa <minatogawavlf@gmail.com>
2023-06-14 01:06:51 -0400

    run build in react

--- backend/app.js
+++ backend/app.js
@@ -4,12 +4,13 @@ require('dotenv').config({ path: './.env' })
 
 
 const express = require('express');
+const path = require('path');
 const mongoose = require('mongoose');
 const app = express();
 const cors = require('cors');
 const connectDB = require('./db');
 const pokemonRoutes = require('./routes/pokemonRoutes');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 
@@ -17,6 +18,14 @@ connectDB();
 
 app.use(express.json());
 app.use(pokemonRoutes);
+
+// Serve any static files built by React
+app.use(express.static(path.join(__dirname, 'build')));
+
+// Handle React routing, return all requests to the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
